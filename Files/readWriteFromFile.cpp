04ae72978107fe7421483356c13fd84e1a705c52
Ramit Kovvalpurail <rkovvalpurail@yodlee.com>
2019-03-14 18:47:45 +0530

    Adding code for reading and writing into files

---
+++ Files/readWriteFromFile.cpp
@@ -0,0 +1,68 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <numeric>
+#include <cmath>
+#include <sstream>
+ 
+// iostream allows use to read from the standard 
+// input (keyboard) and write to the standard output
+// (console)
+// fstream is needed for working with files
+#include <fstream>
+ 
+// ----- PROBLEM FUNCTION PROTOTYPE -----
+ 
+std::vector<std::string> StringToVector(std::string, 
+        char separator);
+ 
+// ----- END OF PROBLEM FUNCTION PROTOTYPE -----
+ 
+int main()
+{
+    std::ofstream writeToFile;
+    std::ifstream readFromFile;
+    std::string txtToWrite = "";
+    std::string txtFromFile = "";
+    
+    // We open the file by providing a name and then either
+    // ios::app : Append to the end of the file
+    // ios::trunc : If the exists delete content
+    // ios::in : Open file for reading
+    // ios::out : Open file for writing
+    // ios::ate : Open writing and move to the end of the file
+    writeToFile.open("test.txt", std::ios_base::out | std::ios_base::trunc);
+    
+    if(writeToFile.is_open()){
+        // You can write with the stream insertion operator
+        writeToFile << "Beginning of File\n";
+ 
+        // You can write data in a string
+        std::cout << "Enter data to write : ";
+        getline(std::cin, txtToWrite);
+        writeToFile << txtToWrite;
+        
+        // Close the file 
+        writeToFile.close();
+    }
+    
+    // Open the file for reading
+    readFromFile.open("test.txt", std::ios_base::in);
+    
+    if(readFromFile.is_open()){
+        
+        // Read text from file
+        while(readFromFile.good()){
+            getline(readFromFile, txtFromFile);
+        
+            // Print text from file
+            std::cout << txtFromFile << "\n";
+        }   
+        readFromFile.close();
+    }
+    
+    return 0;
+}
+ 
